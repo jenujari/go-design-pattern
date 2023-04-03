3f6aead2d3f2597a0aa70e4b288560778a8a58a2
Jenish Jariwala <jhon.5456@gmail.com>
2023-04-03 14:54:41 +0530

    Bridge

---
+++ Bridge/main.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var macComputer Computer
+	var winComputer Computer
+
+	hpPrinter := &Hp{}
+	epsonPrinter := &Epson{}
+
+	macComputer = &Mac{}
+
+	macComputer.SetPrinter(hpPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	macComputer.SetPrinter(epsonPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	winComputer = &Windows{}
+
+	winComputer.SetPrinter(hpPrinter)
+	winComputer.Print()
+	fmt.Println()
+
+	winComputer.SetPrinter(epsonPrinter)
+	winComputer.Print()
+	fmt.Println()
+}
+
+type Hp struct {
+}
+
+func (p *Hp) PrintFile() {
+	fmt.Println("Printing by a HP Printer")
+}
+
+type Epson struct {
+}
+
+func (p *Epson) PrintFile() {
+	fmt.Println("Printing by a EPSON Printer")
+}
+
+type Printer interface {
+	PrintFile()
+}
+
+type Windows struct {
+	printer Printer
+}
+
+func (w *Windows) Print() {
+	fmt.Println("Print request for windows")
+	w.printer.PrintFile()
+}
+
+func (w *Windows) SetPrinter(p Printer) {
+	w.printer = p
+}
+
+type Mac struct {
+	printer Printer
+}
+
+func (m *Mac) Print() {
+	fmt.Println("Print request for mac")
+	m.printer.PrintFile()
+}
+
+func (m *Mac) SetPrinter(p Printer) {
+	m.printer = p
+}
+
+type Computer interface {
+	Print()
+	SetPrinter(Printer)
+}

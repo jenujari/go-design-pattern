50348ad279757f9458599d8ebb3ab783fe03a78c
Jenish Jariwala <jhon.5456@gmail.com>
2023-04-03 16:30:08 +0530

    Decorator

---
+++ Decorator/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// https://refactoring.guru/design-patterns/decorator
+func main() {
+
+	pizza := &VeggeMania{}
+
+	//Add cheese topping
+	pizzaWithCheese := &CheeseTopping{
+		pizza: pizza,
+	}
+
+	//Add tomato topping
+	pizzaWithCheeseAndTomato := &TomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+
+	fmt.Printf("Price of veggeMania with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
+}
+
+type CheeseTopping struct {
+	pizza IPizza
+}
+
+func (c *CheeseTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 10
+}
+
+type TomatoTopping struct {
+	pizza IPizza
+}
+
+func (c *TomatoTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 7
+}
+
+type VeggeMania struct {
+}
+
+func (p *VeggeMania) getPrice() int {
+	return 15
+}
+
+type IPizza interface {
+	getPrice() int
+}
